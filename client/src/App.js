037dfc50e3145d6d1cd96c4642cfbbbdb78229a8
Danny Batarseh <danny@Dannys-MacBook-Pro.local>
2018-08-07 12:46:38 -0700

    more changes

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={car} className="App-logo" alt="logo" />
+          <img src={car} alt="logo" />
           <h1 className="App-title">Find your ride!</h1>
         </header>
         <p className="App-intro">{this.state.response}</p>
