b8e7497f1cec49424a496c6193412672b0dafb3f
Danny Batarseh <danny@Dannys-MacBook-Pro.local>
2018-08-08 11:58:54 -0700

    mlab

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,39 @@ import car from './Aventador-Transparent-Background.png';
 
 import './App.css';
 
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+
+
 class App extends Component {
   state = {
     response: ''
@@ -56,6 +89,7 @@ class App extends Component {
         <div>
           <p className="App-intro">{this.state.response}</p>
         </div>
+        <Nameform />
       </div>
     );
   }
--- server.js
+++ server.js
@@ -2,10 +2,32 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
+var fs = require("fs")
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://Danny:Danny_97@ds215822.mlab.com:15822/carpool');
+
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
 // API calls
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Coming soon!' });
 });
+
+//Test endpoint
+app.get('/api/rides', (req, res) => {
+  fs.readFile('input.txt', function (err, data) {
+    if (err) {
+       return console.error(err);
+    }
+    res.send("Asynchronous read: ");
+ });
+});
+
+
+
+
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
